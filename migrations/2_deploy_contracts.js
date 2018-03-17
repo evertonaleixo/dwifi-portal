1b4946a22b4262c71a7793cd522b32d808ed9665
Everton <everton.jiujitsu@gmail.com>
2018-03-17 17:16:44 -0300

    add contracts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,14 @@
 var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
 var Authentication = artifacts.require("./Authentication.sol");
+var Dwifi = artifacts.require("./Dwifi.sol");
+var EIP20Interface = artifacts.require("./EIP20Interface.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
   deployer.link(Ownable, Killable);
   deployer.deploy(Killable);
   deployer.link(Killable, Authentication);
-  deployer.deploy(Authentication);
+  deployer.deploy(Dwifi);
+  deployer.deploy(EIP20Interface);
 };
